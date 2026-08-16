a3293cb8855f793abe584f730dc03d04a7b57e42
nozaki-shota11 <nzk.shota11@gmail.com>
2026-08-16 17:59:19 +0900

    Initial setup for Cloudflare Pages

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-white dark:bg-black text-black dark:text-white">
+      <h1 className="text-4xl font-bold">Shared Map App - 開発スタート！</h1>
+    </div>
+  );
+}
